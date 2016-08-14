834c807288e4592ff318adc2141c744ff5cb2913
Nolan Waite <nolan@nolanw.ca>
2016-08-14 20:47:40 -0300

    Migrate playground to Swift 3 and project to Xcode 8 (beta 5).

--- HTMLReader.playground/Contents.swift
+++ HTMLReader.playground/Contents.swift
@@ -1,26 +1,25 @@
 //: HTMLReader – A WHATWG-compliant HTML parser
 import HTMLReader
-import XCPlayground
+import PlaygroundSupport
 
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 let homepage = "https://github.com/nolanw/HTMLReader"
-NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: homepage)!) {
+URLSession.shared.dataTask(with: URL(string: homepage)!) {
     (data, response, error) in
     var contentType: String? = nil
-    if let response = response as? NSHTTPURLResponse {
+    if let response = response as? HTTPURLResponse {
         contentType = response.allHeaderFields["Content-Type"] as? String
     }
     if let data = data {
         let home = HTMLDocument(data: data, contentTypeHeader:contentType)
-        if let div = home.firstNodeMatchingSelector(".repository-meta-content") {
-            let whitespace = NSCharacterSet.whitespaceAndNewlineCharacterSet()
-            print(div.textContent.stringByTrimmingCharactersInSet(whitespace))
+        if let div = home.firstNode(matchingSelector: ".repository-meta-content") {
+            print(div.textContent.trimmingCharacters(in: .whitespacesAndNewlines))
         } else {
             print("Failed to match .repository-meta-content, maybe the HTML changed?")
         }
     } else {
         print("No data received, sorry.")
     }
-    XCPlaygroundPage.finishExecution(XCPlaygroundPage.currentPage)
+    PlaygroundPage.finishExecution(PlaygroundPage.current)
 }.resume()
